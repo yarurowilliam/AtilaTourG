98b45c1d48dc5b3fa0e20ab9971bce6886dac495
Johan <johan159753456@outlook.com>
2020-06-13 13:01:57 -0500

    Añadida mas activitades a valledupar

--- app/src/main/java/com/example/atilaversionbeta/MainActivity.java
+++ app/src/main/java/com/example/atilaversionbeta/MainActivity.java
@@ -262,6 +262,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         guardarCiclomonta();
         guardarCaminata();
         guardarArtesania();
+        guardarParranda();
+        guardarNadarRio();
     }
 
     public void guardarCiclomonta(){
@@ -306,7 +308,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         values.put(AtilaBD.CODIGO_ACTIVIDAD,1);
         values.put(AtilaBD.MUNICIPIO_ACTIVIDAD,"Valledupar");
         values.put(AtilaBD.NOMBRE_ACTIVIDAD,"Artesania");
-        values.put(AtilaBD.INFO_ACTIVIDAD,"En los municipos de Pueblo Bello, Chimichagua y Valledupar puede encontrar artesanías locales como mochilas arhuacas y kankuamas, esteras y collares. Además de las populares hamacas y artículos propios de la región perfectos como souvenirs.");
+        values.put(AtilaBD.INFO_ACTIVIDAD,"En Valledupar puede encontrar artesanías locales como mochilas arhuacas y kankuamas, esteras y collares.");
         values.put(AtilaBD.FOTO_ACTIVIDAD, R.drawable.senderismo);
         values.put(AtilaBD.IMG_DETALLE_ACTIVIDAD, R.drawable.senderdescrip);
         values.put(AtilaBD.DESCRIPCION_ACTIVIDAD, "Artesanía se refiere al trabajo de un artesano o artesana (normalmente realizado de forma manual por una persona, sin el auxilio de maquinaria o automatizaciones), como al objeto o producto obtenido en el que cada pieza es distinta a las demás. La artesanía como actividad material se diferencia del trabajo en serie o industrial. Para que una artesanía sea tal debe ser trabajada a mano y cuanto menos procesos industriales tenga, más artesanal va a ser. La artesanía es un objeto totalmente cultural, ya que tiene la particularidad de variar dependiendo del contexto social, el paisaje, el clima, los recursos y la historia del lugar donde se realiza.");
@@ -317,6 +319,62 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         db.insert(AtilaBD.TABLA_ACTIVIDAD, null, values);
     }
+
+    public void guardarParranda(){
+        SQLiteDatabase db = actividadSave.getWritableDatabase();
+        ContentValues values =  new ContentValues();
+        values.put(AtilaBD.CODIGO_ACTIVIDAD,2);
+        values.put(AtilaBD.MUNICIPIO_ACTIVIDAD,"Valledupar");
+        values.put(AtilaBD.NOMBRE_ACTIVIDAD,"Parranda Vallenata");
+        values.put(AtilaBD.INFO_ACTIVIDAD,"¡Viva una verdadera Parranda Vallenata en la tierra del vallenato!");
+        values.put(AtilaBD.FOTO_ACTIVIDAD, R.drawable.senderismo);
+        values.put(AtilaBD.IMG_DETALLE_ACTIVIDAD, R.drawable.senderdescrip);
+        values.put(AtilaBD.DESCRIPCION_ACTIVIDAD, "La Parranda es, fundamentalmente, un rito de amistad. La más alta celebración que se hace por motivos muy especiales y en honor de alguien o algo que se quiere exaltar. Los contertulios se sitúan alrededor del conjunto de música vallenata y en medio de gran silencio y atención escuchan los cantos que éste interpreta. Entre canto y canto aparecen los chistes, las anécdotas, los cuentos, las historias que dieron origen a algunos cantos, mientras el licor circula profusamente entre los invitados que finalmente, bien avanzadas las horas, acaban degustando el suculento sancocho que ha estado hirviendo en los fogones debajo del confortable palo de mango que está en el patio de la casa.");
+        values.put(AtilaBD.URLINFO_ACTIVIDAD, "https://festivalvallenato.com/parranda-v/#:~:text=La%20parranda%20Vallenata&text=La%20más%20alta%20celebración%20que,los%20cantos%20que%20éste%20interpreta.");
+        values.put(AtilaBD.LUGARES_ACTIVIDAD, "- Guacaó\n- La placita\n- Tierra de cantores\n- Plaza Alfonso López\n- Escuela del Turco Gil\n- Compai Chipuco");
+        values.put(AtilaBD.URLMAPS_ACTIVIDAD, "https://www.google.com/maps/place/La+Placita+Bar/@10.4489892,-73.2527739,14z/data=!4m8!1m2!2m1!1sLa+placita!3m4!1s0x8e8ab9b3ea7053ab:0xcd4aec30e5b3bff3!8m2!3d10.4774918!4d-73.2456108");
+
+
+        db.insert(AtilaBD.TABLA_ACTIVIDAD, null, values);
+    }
+
+    public void guardarNadarRio(){
+        SQLiteDatabase db = actividadSave.getWritableDatabase();
+        ContentValues values =  new ContentValues();
+        values.put(AtilaBD.CODIGO_ACTIVIDAD,3);
+        values.put(AtilaBD.MUNICIPIO_ACTIVIDAD,"Valledupar");
+        values.put(AtilaBD.NOMBRE_ACTIVIDAD,"Bañarse en el Rio");
+        values.put(AtilaBD.INFO_ACTIVIDAD,"Valledupar cuenta con una riqueza hídrica que puede aprovechar para darse un refrescante chapuzón.");
+        values.put(AtilaBD.FOTO_ACTIVIDAD, R.drawable.senderismo);
+        values.put(AtilaBD.IMG_DETALLE_ACTIVIDAD, R.drawable.senderdescrip);
+        values.put(AtilaBD.DESCRIPCION_ACTIVIDAD, "A su paso por el norte de Valledupar se encuentra el balneario de Hurtado, principal lugar de recreación y diversión de la capital del Cesar. En este mismo sitio se encuentran el Pueblito Vallenato y el parque Lineal. Además, alimenta de agua al acueducto de dicha ciudad.");
+        values.put(AtilaBD.URLINFO_ACTIVIDAD, "https://es.wikipedia.org/wiki/Río_Guatapurí");
+        values.put(AtilaBD.LUGARES_ACTIVIDAD, "- Río Guatapurío\n- La Mina\n- Chemesquemena\n- La Mesa\n- Ría Badillo\n- La Vega\n- El Mojao");
+        values.put(AtilaBD.URLMAPS_ACTIVIDAD, "https://www.google.com/maps/place/Balneario+Hurtado/@10.4489877,-73.2702838,13z/data=!4m8!1m2!2m1!1sbalneario+hurtado!3m4!1s0x8e8abbfede10430b:0xe5ae13c22c036433!8m2!3d10.4235315!4d-73.255595");
+
+
+        db.insert(AtilaBD.TABLA_ACTIVIDAD, null, values);
+    }
+
+    public void guardarCityTour(){
+        SQLiteDatabase db = actividadSave.getWritableDatabase();
+        ContentValues values =  new ContentValues();
+        values.put(AtilaBD.CODIGO_ACTIVIDAD,3);
+        values.put(AtilaBD.MUNICIPIO_ACTIVIDAD,"Valledupar");
+        values.put(AtilaBD.NOMBRE_ACTIVIDAD,"City Tour");
+        values.put(AtilaBD.INFO_ACTIVIDAD,"Valledupar es reconocido por sus monumentos y glorietas, en homenaje a su historia y cultura. En la ciudad hay más de 20 monumentos y glorietas, la mayoría dedicados a la música y cultura tradicional vallenata.");
+        values.put(AtilaBD.FOTO_ACTIVIDAD, R.drawable.senderismo);
+        values.put(AtilaBD.IMG_DETALLE_ACTIVIDAD, R.drawable.senderdescrip);
+        values.put(AtilaBD.DESCRIPCION_ACTIVIDAD, "A su paso por el norte de Valledupar se encuentra el balneario de Hurtado, principal lugar de recreación y diversión de la capital del Cesar. En este mismo sitio se encuentran el Pueblito Vallenato y el parque Lineal. Además, alimenta de agua al acueducto de dicha ciudad.");
+        values.put(AtilaBD.URLINFO_ACTIVIDAD, "https://es.wikipedia.org/wiki/Río_Guatapurí");
+        values.put(AtilaBD.LUGARES_ACTIVIDAD, "- Río Guatapurío\n- La Mina\n- Chemesquemena\n- La Mesa\n- Ría Badillo\n- La Vega\n- El Mojao");
+        values.put(AtilaBD.URLMAPS_ACTIVIDAD, "https://www.google.com/maps/place/Balneario+Hurtado/@10.4489877,-73.2702838,13z/data=!4m8!1m2!2m1!1sbalneario+hurtado!3m4!1s0x8e8abbfede10430b:0xe5ae13c22c036433!8m2!3d10.4235315!4d-73.255595");
+
+
+        db.insert(AtilaBD.TABLA_ACTIVIDAD, null, values);
+    }
+
+
 
 
     public void guardarParapente(){
